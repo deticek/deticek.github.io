3497afff1310b080a17a44e7d042ef1bdd0ece14
deticek <98918604+deticek@users.noreply.github.com>
2024-04-06 21:16:37 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,85 @@
+/*!
+* Start Bootstrap - Grayscale v7.0.6 (https://startbootstrap.com/theme/grayscale)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+window.addEventListener('scroll', function() {
+    var paragraphs = document.querySelectorAll('p');
+    var headings = document.querySelectorAll('h3');
+    var images = document.querySelectorAll('img');
+  
+    // Prikazovanje <p> elementov
+    paragraphs.forEach(function(paragraph) {
+      var position = paragraph.getBoundingClientRect();
+      if (position.top < window.innerHeight && position.bottom >= 0) {
+        paragraph.style.opacity = '1';
+      }
+    });
+  
+    // Prikazovanje <h3> elementov
+    headings.forEach(function(heading) {
+      var position = heading.getBoundingClientRect();
+      if (position.top < window.innerHeight && position.bottom >= 0) {
+        heading.style.opacity = '1';
+      }
+    });
+  
+    // Prikazovanje <img> elementov
+    images.forEach(function(image) {
+      var position = image.getBoundingClientRect();
+      if (position.top < window.innerHeight && position.bottom >= 0) {
+        image.style.opacity = '1';
+      }
+    });
+  });
+  
